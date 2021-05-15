2d7d84857355a2a8bf970fe8a7725e68e9c2f7bf
ByteException_ <git@byteexception.eu>
2021-05-15 02:06:59 +0200

    Add DynamicIcon support
    Signed-off-by: ByteException_ <git@byteexception.eu>

--- src/main/java/eu/byteexception/labymod/OPSuchtLabyModAddon.java
+++ src/main/java/eu/byteexception/labymod/OPSuchtLabyModAddon.java
@@ -1,13 +1,12 @@
 package eu.byteexception.labymod;
 
+import eu.byteexception.labymod.gui.icondata.DynamicIconData;
 import eu.byteexception.labymod.listener.SettingsSynchronisationListener;
 import eu.byteexception.labymod.server.OPSuchtLabyModServer;
 import net.labymod.api.LabyModAddon;
 import net.labymod.ingamegui.ModuleCategory;
 import net.labymod.ingamegui.ModuleCategoryRegistry;
-import net.labymod.settings.elements.ControlElement.IconData;
 import net.labymod.settings.elements.SettingsElement;
-import net.labymod.utils.Material;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -16,7 +15,8 @@ public class OPSuchtLabyModAddon extends LabyModAddon {
 
     public final Logger logger = Logger.getLogger("Minecraft");
 
-    public final ModuleCategory opSuchtModuleCategory = new ModuleCategory("OPSucht", true, new IconData(Material.GRASS_PATH));
+    public final ModuleCategory opSuchtModuleCategory = new ModuleCategory("OPSucht", true,
+            new DynamicIconData("opsucht_logo", "https://files.byteexception.eu/img/v26EtegKIU.png"));
 
     @Override
     public void onEnable() {
