68341a9eb8cbc66fbe01c2861cb54fb6d067bc92
ByteException_ <git@byteexception.eu>
2021-05-17 03:42:57 +0200

    Fix module specified listener registering
    Signed-off-by: ByteException_ <git@byteexception.eu>

--- src/main/java/eu/byteexception/labymod/internal/listener/PlayerSettingsSynchronizeListener.java
+++ src/main/java/eu/byteexception/labymod/internal/listener/PlayerSettingsSynchronizeListener.java
@@ -6,6 +6,9 @@ import eu.byteexception.labymod.internal.events.PlayerSettingsSynchronizeEvent;
 import lombok.AllArgsConstructor;
 import net.labymod.api.event.Subscribe;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 public class PlayerSettingsSynchronizeListener {
 
@@ -14,5 +17,19 @@ public class PlayerSettingsSynchronizeListener {
     @Subscribe
     public void onPlayerSettingsSynchronize(PlayerSettingsSynchronizeEvent event) {
         PlayerSettings.playerSettings = event.getPlayerSettings();
+
+        this.addon.getModuleListener().stream().filter(listenerClazz -> this.addon.getModules().stream().filter(labyModule -> Objects.nonNull(labyModule.getListenerName()))
+                .anyMatch(labyModule -> labyModule.getListenerName().equals(listenerClazz) && event.getPlayerSettings().get(labyModule.getSettingName()).getAsBoolean())).map(clazz -> {
+            try {
+                return Class.forName(clazz, true, Thread.currentThread().getContextClassLoader()).newInstance();
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).filter(Objects::nonNull).collect(Collectors.toList()).forEach(listener -> {
+            this.addon.getModuleListener().remove(listener.getClass().getCanonicalName());
+            this.addon.getApi().getEventService().registerListener(listener);
+            this.addon.getLogger().info("Registering listener " + listener.getClass().getSimpleName());
+        });
     }
 }
