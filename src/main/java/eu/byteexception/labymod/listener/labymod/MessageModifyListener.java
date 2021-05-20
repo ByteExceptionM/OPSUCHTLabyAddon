5712a5f1e63836356cd074473d2e1619564d43cb
ByteException_ <git@byteexception.eu>
2021-05-20 15:24:23 +0200

    Code reformat
    Signed-off-by: ByteException_ <git@byteexception.eu>

--- src/main/java/eu/byteexception/labymod/listener/labymod/MessageModifyListener.java
+++ src/main/java/eu/byteexception/labymod/listener/labymod/MessageModifyListener.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.events.client.chat.MessageModifyEvent;
 import net.labymod.utils.ModColor;
-import net.minecraft.util.text.*;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextComponentUtils;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.ClickEvent.Action;
 import net.minecraft.util.text.event.HoverEvent;
@@ -47,14 +49,16 @@ public class MessageModifyListener {
             playerName = playerNameBuilder.substring(0);
         }
 
-        Style style = playerNameComponent.getStyle()
-                .setClickEvent(new ClickEvent(Action.RUN_COMMAND, "/realname " + playerName))
-                .setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new StringTextComponent(ModColor.GREEN + "Klicke, für den richtigen Namen")));
-
-        IFormattableTextComponent textComponent = TextComponentUtils.func_240648_a_(playerNameComponent.copyRaw(), playerNameComponent.getStyle().mergeStyle(style));
-
-        component.getSiblings().set(componentSiblings.indexOf(playerNameComponent), textComponent);
+        component.getSiblings().set(componentSiblings.indexOf(playerNameComponent), this.applyStyle(playerNameComponent, playerName));
 
         event.setComponent(component);
+    }
+
+    private ITextComponent applyStyle(ITextComponent textComponent, String playerName) {
+        return TextComponentUtils.func_240648_a_(textComponent.copyRaw(), textComponent.getStyle().setClickEvent(null).setHoverEvent(null).mergeStyle(
+                textComponent.getStyle()
+                        .setClickEvent(new ClickEvent(Action.RUN_COMMAND, "/realname " + playerName))
+                        .setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new StringTextComponent(ModColor.GREEN + "Klicke, für den richtigen Namen")))
+        ));
     }
 }
